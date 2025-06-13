b5b64c88c624b87410c09b1bb3e13286f2835bc0
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-06-13 06:56:53 -0300

    Start

---
+++ api/index.php
@@ -0,0 +1,59 @@
+<?php
+
+ob_start();
+
+require  __DIR__ . "/../vendor/autoload.php";
+
+// os headers abaixo são necessários para permitir o acesso a API por clientes externos ao domínio
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header('Access-Control-Allow-Credentials: true'); // Permitir credenciais
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+use CoffeeCode\Router\Router;
+
+$route = new Router("http://localhost:8080/acme-manha/api",":");
+
+$route->namespace("Source\WebService");
+
+/* USERS */
+
+$route->group("/users");
+
+$route->post("/login", "Users:login");
+
+$route->get("/", "Users:listUsers");
+$route->get("/id/{id}", "Users:listUserById");
+$route->get("/id/", "Users:listUserById");
+$route->post("/add", "Users:createUser");
+$route->put("/update", "Users:updateUser");
+
+$route->group("null");
+
+$route->group("/products");
+
+$route->delete("/delete/id/{id}", "Products:deleteProduct");
+
+$route->group("null");
+
+$route->dispatch();
+
+/** ERROR REDIRECT */
+if ($route->error()) {
+    header('Content-Type: application/json; charset=UTF-8');
+    http_response_code(404);
+
+    echo json_encode([
+        "code" => 404,
+        "status" => "not_found",
+        "message" => "URL não encontrada"
+    ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+}
+
+ob_end_flush();
